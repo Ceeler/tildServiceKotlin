430b169b7bb9d40c4a65ffbc63d578a3ba9f5fb8
Danil Losev <u4iha.danil@yandex.ru>
2023-09-16 20:23:05 +0300

    feat: add user simple controller

--- src/main/kotlin/space/tild/tildservicekotlin/controller/UserController.kt
+++ src/main/kotlin/space/tild/tildservicekotlin/controller/UserController.kt
@@ -1,4 +1,25 @@
 package space.tild.tildservicekotlin.controller
 
-class UserController {
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+import space.tild.tildservicekotlin.models.UserDto
+import space.tild.tildservicekotlin.service.UserService
+
+
+@RestController
+@RequestMapping("/user")
+class UserController (
+    var userService: UserService
+        ){
+
+    @PostMapping("/signup")
+    fun userSignup(@RequestBody userDto : UserDto) = userService.userSignup(userDto)
+
+    @GetMapping("/{id}")
+    fun getUserById(@PathVariable id : Long) = userService.getUserById(id)
 }
