9ab14d1a41f3bb76b18cdbbf9dd6a7589836ea2e
Danil Losev <u4iha.danil@yandex.ru>
2023-09-16 20:18:46 +0300

    feat: messages entities init

--- src/main/kotlin/space/tild/tildservicekotlin/database/Messages.kt
+++ src/main/kotlin/space/tild/tildservicekotlin/database/Messages.kt
@@ -1,20 +1,46 @@
 package space.tild.tildservicekotlin.database
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.SequenceGenerator
+import jakarta.persistence.Table
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
 import java.time.Instant
 
+@Entity
+@Table(name = "direct_messages")
 data class DirectMessage(
-    var id : Long,
-    var messageText : String,
-    var author : User,
-    var receiver : User,
-    var createdAt : Instant
+    @Id @GeneratedValue(strategy = GenerationType.AUTO, generator = "direct_messages_id_seq")
+    @SequenceGenerator(name = "direct_messages_id_seq", sequenceName = "direct_messages_id_seq", allocationSize = 1)
+    var id : Long? = null,
+    var messageText : String = "Empty",
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    var author : User = User(),
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    var receiver : User = User(),
+    @CreationTimestamp var createdAt : Instant? = null
 )
 
+@Entity
+@Table(name = "project_messages")
 data class ProjectMessage(
-    var id : Long,
-    var projectText : String,
-    var author : User,
-    var project : Project,
-    var createdAt : Instant,
-    var updatedAt : Instant
+    @Id @GeneratedValue(strategy = GenerationType.AUTO, generator = "project_messages_id_seq")
+    @SequenceGenerator(name = "project_messages_id_seq", sequenceName = "project_messages_id_seq", allocationSize = 1)
+    var id : Long? = null,
+    var projectText : String = "Empty",
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    var author : User = User(),
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    var project: Project = Project(),
+    @CreationTimestamp var createdAt : Instant? = null,
+    @UpdateTimestamp var updatedAt : Instant? = null
 )
