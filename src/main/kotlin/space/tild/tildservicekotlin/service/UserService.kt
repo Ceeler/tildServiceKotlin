7f947ca564d638bf839c21dc08572c403666caa7
Danil Losev <u4iha.danil@yandex.ru>
2023-09-16 20:22:21 +0300

    feat: user service implementation

--- src/main/kotlin/space/tild/tildservicekotlin/service/UserService.kt
+++ src/main/kotlin/space/tild/tildservicekotlin/service/UserService.kt
@@ -1,4 +1,23 @@
 package space.tild.tildservicekotlin.service
 
-class UserService {
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import space.tild.tildservicekotlin.database.User
+import space.tild.tildservicekotlin.repository.UserRepository
+import space.tild.tildservicekotlin.models.UserDto
+
+@Service
+class UserService(val userRepository: UserRepository) {
+
+    fun userSignup(userDto: UserDto) : ResponseEntity<User> {
+        val newUser = User(firstName = userDto.firstName, username = userDto.username, email = userDto.email, password = userDto.password)
+        //val createdUser = userRepository.save(newUser).block()
+        val createdUser = userRepository.save(newUser)
+        return ResponseEntity(createdUser, HttpStatus.CREATED)
+    }
+    fun getUserById(id : Long) : ResponseEntity<User> {
+        val user = userRepository.findById(id)
+        return ResponseEntity(user.get(), HttpStatus.OK)
+    }
 }
